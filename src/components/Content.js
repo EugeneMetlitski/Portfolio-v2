ccd09b045bc8017d07595fa454f2335453b27182
Eugene Metlitski <evguenimetlitski@outlook.com>
2019-11-22 00:52:17 +1000

    Skills & Education Done

--- src/components/Content.js
+++ src/components/Content.js
@@ -11,6 +11,14 @@ import ExpansionPanel from '@material-ui/core/ExpansionPanel';
 import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
 import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import Chip from '@material-ui/core/Chip';
+import Paper from '@material-ui/core/Paper';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import Avatar from '@material-ui/core/Avatar';
+
 
 export default function Content() {
   const classes = useStyles();
@@ -69,25 +77,169 @@ export default function Content() {
               semester and during other semesters I undertook 4 subject per semester (Also
               having done Diploma in Tafe gave credits for some subjects and I had to do less
               subjects in the degree because of that).<br /><br />
-              I was born in Russia live in Australia since age 12 (I'm 31 years old now),
+              I was born in Russia and have lived in Australia since the age of 12 (I'm 31 years old now),
               therefore I'm fluent in English language. <br /><br />
-              I enjoy developing software and writing code wether it is to make a website,
-              front-end, back-end, mobile, make video-game, or write low level c/c++ code. I'm
-              not an expert in any language yet, but have some experience with making many
-              different types of small programs.
+              I enjoy developing software and writing code whether it is to make a website
+              (front-end and/or back-end) mobile app, video-game, do some data analysis, machine learning
+              or write a program in low level c/c++ code. I'm not an expert in any language yet, but have
+              some experience with making many different types of small programs (mostly from Tafe
+              and University).
             </Typography>
           </ExpansionPanelDetails>
         </ExpansionPanel>
         </div>
 
-        <br /><Divider variant='middle' /><br />
+
 
         {/* SKILLS AND EDUCATION */}
+        <br /><Divider variant='middle' /><br />
         <Typography className={classes.title} gutterBottom variant="h5" component="h2">
           Skills and Education
         </Typography>
         
+        <br />
+        <Typography className={classes.caption} variant="caption" display="block" gutterBottom>
+          These are the <b>languages</b> that I have experience with.
+        </Typography>
 
+        <Paper className={classes.chips}>
+          <Chip className={classes.chip} label="JavaScript" />
+          <Chip className={classes.chip} label="CSS" />
+          <Chip className={classes.chip} label="HTML" />
+          <Chip className={classes.chip} label="C#" />
+          <Chip className={classes.chip} label="Java" />
+          <Chip className={classes.chip} label="C/C++" />
+          <Chip className={classes.chip} label="SQL" />
+          <Chip className={classes.chip} label="Python" />
+          <Chip className={classes.chip} label="Matlab" />
+          <Chip className={classes.chip} label="Lua" />
+        </Paper>
+
+        {/* <br /> */}
+        <Typography className={classes.caption} variant="caption" display="block" gutterBottom>
+          These are the <b>technologies</b> that I have experience with.
+        </Typography>
+
+        <Paper className={classes.chips}>
+          <Chip className={classes.chip} label="React" />
+          <Chip className={classes.chip} label="Angular" />
+          <Chip className={classes.chip} label=".NET Core 2.0" />
+          <Chip className={classes.chip} label="SQL Server" />
+          <Chip className={classes.chip} label="Web Assembly" />
+        </Paper>
+
+        {/* EDUCATION LIST */}
+        <br />
+        <Typography className={classes.caption} variant="caption" display="block" gutterBottom>
+          <b>Education</b>
+        </Typography>
+        <Paper className={classes.education}>
+          <List className={classes.educationList}>
+            <ListItem className={classes.educationItem} alignItems="flex-start">
+              <ListItemAvatar>
+                <Avatar alt="QUT Logo" src="" />
+              </ListItemAvatar>
+              <ListItemText
+                primary="Bachelor of Information Technology (Feb 2016 - June 2021)"
+                secondary={
+                  <React.Fragment>
+                    <Typography
+                      component="span"
+                      variant="body2"
+                      className={classes.educationInline}
+                      color="textPrimary"
+                    >
+                      Queensland University of Technology<br />
+                    </Typography>
+                    {"Major - Computer Sciene"}<br />
+                    {"Minor - Data Centric Computing (Data Analysis)"}<br />
+                    {"Minor - Machine Learning"}
+                  </React.Fragment>
+                }
+              />
+            </ListItem>
+            <Divider variant="inset" component="li" />
+            <ListItem className={classes.educationItem} alignItems="flex-start">
+              <ListItemAvatar>
+                <Avatar alt="Tafe Logo" src="" />
+              </ListItemAvatar>
+              <ListItemText
+                primary="Diploma of Software Development (Feb 2016 - Feb 2017)"
+                secondary={
+                  <React.Fragment>
+                    <Typography
+                      component="span"
+                      variant="body2"
+                      className={classes.educationInline}
+                      color="textPrimary"
+                    >
+                      Queensland Tafe
+                    </Typography>
+                  </React.Fragment>
+                }
+              />
+            </ListItem>
+            <Divider variant="inset" component="li" />
+            <ListItem className={classes.educationItem} alignItems="flex-start">
+              <ListItemAvatar>
+                <Avatar alt="Tafe Logo" src="" />
+              </ListItemAvatar>
+              <ListItemText
+                primary="Certificate 3 in Networking (Sep 2015 - Nov 2015)"
+                secondary={
+                  <React.Fragment>
+                    <Typography
+                      component="span"
+                      variant="body2"
+                      className={classes.educationInline}
+                      color="textPrimary"
+                    >
+                      Queensland Tafe
+                    </Typography>
+                  </React.Fragment>
+                }
+              />
+            </ListItem>
+          </List>
+        </Paper>
+
+        <br />
+        {/* Expansion Pannel: Skills and Education */}
+        <div className={classes.expansionPanelContainer}>
+        <ExpansionPanel>
+          <ExpansionPanelSummary className={classes.expansionPanelSummary}
+            expandIcon={<ExpandMoreIcon className={classes.expansionButton} />}
+            aria-controls="panel1a-content"
+            id="panel1a-header"
+          >
+            <Typography className={classes.expansionPanelHeading}>
+              More Info
+            </Typography>
+          </ExpansionPanelSummary>
+          <ExpansionPanelDetails className={classes.expansionPanelDetails}>
+            <Typography>
+              Since discovering the world of IT 4 years ago, I've completed Certificate 3 in
+              Networking, Diploma of Software Development and almost finished Bachelor of
+              Information Technology degree with 1 subject left to complete next year. The
+              reason why I have 1 subject left to complete is because I've changed my second
+              major half way through the study from computationial science to 2 minors in
+              machine learning and datacentric computing, and this added a few subject to the
+              degree. Another reason is that during some semestors I undertook 3 subjects per
+              semester and during other semesters I undertook 4 subject per semester (Also
+              having done Diploma in Tafe gave credits for some subjects and I had to do less
+              subjects in the degree because of that).
+            </Typography>
+          </ExpansionPanelDetails>
+        </ExpansionPanel>
+        </div>
+
+
+
+        {/* PROJECTS */}
+        <br /><Divider variant='middle' /><br />
+        <Typography className={classes.title} gutterBottom variant="h5" component="h2">
+          Projects
+        </Typography>
 
       </div>
 
@@ -133,7 +285,35 @@ const useStyles = makeStyles(theme => ({
     left: '50%',
     transform: 'translate(-50%, 0)',
   },
-  button: {
-
-  }
+  caption: {
+    textAlign: 'center'
+  },
+  chips: {
+    background: '#f0f1f5',
+    margin: '0 40px 10px 40px',
+    padding: theme.spacing(1, 2),
+    display: 'flex',
+    justifyContent: 'center',
+    flexWrap: 'wrap',
+    '& > *': {
+      margin: theme.spacing(0.5),
+    },
+  },
+  chip: {
+    background: '#263238',
+    color: '#dcdee6',
+  },
+  education: {
+    margin: '0 20px 0 20px',
+  },
+  educationList: {
+    background: '#f0f1f5',
+    borderRadius: '4px',
+  },
+  educationItem: {
+    margin: '5px'
+  },
+  educationInline: {
+    display: 'inline',
+  },
 }));
